8ec49a553a712e35202c0c0818a94010ae160633
anton.bilchuk <anton.bilchuk@stylelounge.com>
2018-02-06 21:37:22 +0200

    feat: test upgade

--- src/cacheAPI/cacheService.spec.js
+++ src/cacheAPI/cacheService.spec.js
@@ -1,4 +1,35 @@
 import {expect} from 'chai';
+import sinon from 'sinon';
 import cacheServiceGenerator from './cacheService';
 
-describe("")
+describe("cacheService", () => {
+    let db;
+    let service;
+    beforeEach(() => {
+        const _db = {
+            find: sinon.stub(),
+            findOne: sinon.stub(),
+            count: sinon.stub(),
+            remove: sinon.stub(),
+            update: sinon.stub(),
+        };
+        db = function() {
+            Object.assign(this, _db);
+        };
+        Object.assign(db.prototype, _db);
+
+        service = cacheServiceGenerator(db);
+    });
+        it("get", (done) => {
+        })
+        it("getAllKeys", () => {
+        })
+        it("create", () => {
+        })
+        it("updatev", () => {
+        })
+        it("remove", () => {
+        })
+        it("removeAll", () => {
+        })
+})
